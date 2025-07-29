ffd8fde5f72ae3ac32e399f929952aa86bd9f098
nssamanta <144923305+nssamanta@users.noreply.github.com>
2025-07-29 00:27:22 -0700

    feat: add TodoListItem component and newTodo state - Create TodoListItem component with todo props - Replace li elements with TodoListItem components - Add useState hook to App for newTodo state - Display newTodo state value in paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
 function App() {
+  const [newTodo, setNewTodo] = useState('Example');
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
