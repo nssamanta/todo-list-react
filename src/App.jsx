8a817976923af6a4342d970f61ac593d9d2f8c61
nssamanta <144923305+nssamanta@users.noreply.github.com>
2025-08-19 23:26:57 -0700

    refactor: reorganize project structure into features and shared directories - Create src/features and src/shared directories - Move TodoForm.jsx to src/features - Move TodoList.jsx and TodoListItem.jsx to src/features/TodoList - Update import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from "react";
-import "./App.css";
-import TodoList from "./TodoList";
-import TodoForm from "./TodoForm";
+import { useState } from 'react';
+import './App.css';
+import TodoList from './features/TodoList/TodoList';
+import TodoForm from './features/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
 
-function addTodo(title) {
-  const newTodo = {title, id:Date.now(), isCompleted: false};
-  setTodoList([...todoList, newTodo]);
-}
+  function addTodo(title) {
+    const newTodo = { title, id: Date.now(), isCompleted: false };
+    setTodoList([...todoList, newTodo]);
+  }
 
-function completeTodo(id) {
-  const updatedTodos = todoList.map(todo => {
-    if (todo.id === id) {
-      return {...todo, isCompleted: true};
-    } else {
-      return todo;
-    }
-});
-  setTodoList(updatedTodos);
-}
+  function completeTodo(id) {
+    const updatedTodos = todoList.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      } else {
+        return todo;
+      }
+    });
+    setTodoList(updatedTodos);
+  }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
